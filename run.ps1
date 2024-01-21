f21cc8f3e131f438e8eb07ae976ef95e7969c48f
Crosstyan <cf595833532@gmail.com>
2024-01-22 02:31:31 +0800

    idk

--- run.ps1
+++ run.ps1
@@ -1,1 +1,13 @@
-C:\Users\cross\Desktop\code\clojerl\bin\clojerl.bat -i .\start.clje -e "(main)"
+# $cljerl = "C:\Users\cross\Desktop\code\clojerl\bin\clojerl.bat"
+# # https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_quoting_rules?view=powershell-7.4
+
+# $expr = ($cljerl + " -i server.clje -i start.clje -m clojerl.start")
+# Write-Output $expr
+# Invoke-Expression -Command $expr
+
+$rebar3 = "C:\tools\rebar3\rebar3.ps1"
+
+$expr = ($rebar3 + " clojerl run")
+
+Write-Output $expr
+Invoke-Expression -Command $expr
